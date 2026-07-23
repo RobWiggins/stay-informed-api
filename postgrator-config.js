49b182e3f41f3c97c0c3b368d70d0c9f2dfd6210
robert wiggins <wigginsro11@gmail.com>
2026-07-23 12:55:57 -0400

    fixed automated migrations

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
+const path = require('path');
 
 module.exports = {
-  "migrationDirectory": "migrations",
+  // "migrationDirectory": "migrations",
+  migrationPattern: path.join(__dirname, 'migrations', '*'),
   "driver": "pg",
   "host": process.env.MIGRATION_DB_HOST,
   "port": process.env.MIGRATION_DB_PORT,
   "database": process.env.MIGRATION_DB_NAME,
   "username": process.env.MIGRATION_DB_USER,
-  "password": process.env.MIGRATION_DB_PASS
+  "password": process.env.MIGRATION_DB_PASS,
+
+  schemaTable: 'schemaversion',
 }
